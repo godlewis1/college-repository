eddc61451e9276774a665db925aa2d5b29b7dc93
Luis Fernandez <46386604+godlewis1@users.noreply.github.com>
2020-01-01 22:20:40 -0400

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What’s your name?\n");
+    printf("hello, %s\n", name);
+}
+    
+
+     
+
+   
+  
+   
+
